e60e68db5be8fcf9f661df11ea09e02866e95015
F4bioo <costa.fbo@gmail.com>
2021-11-30 21:10:31 -0300

    Configure modules

--- app/src/main/java/com/fappslab/marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/fappslab/marvel/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fappslab.marvel
+package com.fappslab.marvel.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fappslab.marvel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
