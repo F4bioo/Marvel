db81512d0c0232e151996a2fe586344cd9207e29
F4bioo <costa.fbo@gmail.com>
2021-12-15 23:36:37 -0300

    Create authorization interceptor

--- app/src/main/java/com/fappslab/marvel/framework/di/NetworkModule.kt
+++ app/src/main/java/com/fappslab/marvel/framework/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.fappslab.marvel.framework.di
 
+import com.fappslab.core.data.network.interceptor.AuthorizationInterceptor
 import com.fappslab.marvel.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -9,11 +10,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val TIMEOUT_SECONDS = 15L
 
     @Provides
     fun provideInterceptor(
@@ -26,12 +30,22 @@ object NetworkModule {
     }
 
     @Provides
+    fun provideAuthorizationInterceptor(
+    ): AuthorizationInterceptor = AuthorizationInterceptor(
+        privateKey = BuildConfig.PRIVATE_KEY,
+        publicKey = BuildConfig.PUBLIC_KEY,
+        calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    )
+
+    @Provides
     fun provideOkhttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
     ): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
-        .readTimeout(15, TimeUnit.SECONDS)
-        .connectTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(authorizationInterceptor)
+        .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+        .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
         .build()
 
     @Provides
