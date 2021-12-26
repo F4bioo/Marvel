f3f1a8d0ea23d202e37f720d7a73e986954f0ced
F4bioo <costa.fbo@gmail.com>
2021-12-25 22:06:36 -0300

    Optimise release app

--- app/src/main/java/com/fappslab/marvel/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/fappslab/marvel/framework/network/response/CharacterResponse.kt
@@ -1,10 +1,14 @@
 package com.fappslab.marvel.framework.network.response
 
 import com.fappslab.core.domain.model.Character
+import com.google.gson.annotations.SerializedName
 
 data class CharacterResponse(
+    @SerializedName("id")
     val id: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("thumbnail")
     val thumbnail: ThumbnailResponse
 )
 
--- app/src/main/java/com/fappslab/marvel/framework/network/response/DataWrapperResponse.kt
+++ app/src/main/java/com/fappslab/marvel/framework/network/response/DataWrapperResponse.kt
@@ -1,6 +1,10 @@
 package com.fappslab.marvel.framework.network.response
 
+import com.google.gson.annotations.SerializedName
+
 data class DataWrapperResponse(
+    @SerializedName("copyright")
     val copyright: String,
+    @SerializedName("data")
     val data: DataContainerResponse
 )
